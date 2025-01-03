146c3efc1345809870bffd0ef40cd1f9da3f2f4b
ABHI24033 <abhi24033c@gmail.com>
2025-01-03 08:11:58 +0530

    make design for preview and stepper

--- app/dashboard/loans/personal-loan/personal-loan-form/page.js
+++ app/dashboard/loans/personal-loan/personal-loan-form/page.js
@@ -3,23 +3,17 @@ import React, { useState } from "react";
 import { PersoanlInfo, PersonalLoanFormDetails } from '@/config/PersoanlLoan';
 import CommonForm from "@/components/common/Form";
 import Stepper from '@/components/ui/stepper';
+import Step2 from '@/components/persoanlLon/step2';
+import Step1 from '@/components/persoanlLon/step1';
+import Step3 from '@/components/persoanlLon/step3';
+import FormPreview from '@/components/common/FormPreview';
+import FormPreviewPage from '@/components/common/Previewpage';
+import Formstepper from '@/components/common/Formstepper';
 
-const stepper = (step, formData, setFormData, handleSubmit, prevButton) => {
+const stepper = (step, formData, setFormData, handleSubmit, prevButton, nextButton, showPreview, setShowPreview) => {
     switch (step) {
         case 1: return (
-            <CommonForm
-                formControls={PersonalLoanFormDetails}
-                formData={formData}
-                setFormData={setFormData}
-                buttonText={"Next"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-                step={step}
-                prevButton={prevButton}
-            />
-        )
-        case 2: return (
-            <CommonForm
+            <Step1
                 formControls={PersoanlInfo}
                 formData={formData}
                 setFormData={setFormData}
@@ -27,20 +21,47 @@ const stepper = (step, formData, setFormData, handleSubmit, prevButton) => {
                 onSubmit={handleSubmit}
                 isBtnDisabled={false}
                 step={step}
+                nextButton={nextButton}
                 prevButton={prevButton}
             />
         )
-        // case 3: return (
-        //     <CommonForm
-        //         formControls={PersonalLoanFormDetails}
-        //         formData={formData}
-        //         setFormData={setFormData}
-        //         buttonText={"Next"}
-        //         onSubmit={handleSubmit}
-        //         isBtnDisabled={false}
-        //         step={step}
-        //     />
-        // )
+        case 2: return (
+            <Step2
+                formData={formData}
+                setFormData={setFormData}
+                buttonText={"Next"}
+                onSubmit={handleSubmit}
+                isBtnDisabled={false}
+                step={step}
+                prevButton={prevButton}
+
+            />
+        )
+        case 3: return (
+            <Step3
+                formData={formData}
+                setFormData={setFormData}
+                buttonText={"Next"}
+                onSubmit={handleSubmit}
+                isBtnDisabled={false}
+                step={step}
+                prevButton={prevButton}
+
+            />
+        )
+        case 4: return (
+            <FormPreviewPage
+                showPreview={showPreview}
+                setShowPreview={setShowPreview}
+                // formControls={PersonalLoanFormDetails}
+                // formData={formData}
+                setFormData={setFormData}
+                buttonText={"Next"}
+                onSubmit={handleSubmit}
+                isBtnDisabled={false}
+                step={step}
+            />
+        )
         // case 4: return (
         //     <CommonForm
         //         formControls={PersonalLoanFormDetails}
@@ -60,12 +81,21 @@ const stepper = (step, formData, setFormData, handleSubmit, prevButton) => {
 
 const PersonalLoanForm = () => {
     const [step, setStep] = useState(1);
-    console.log("hello");
 
     const [formData, setFormData] = useState([]);
+    const [showPreview, setShowPreview] = useState(true);
+
+    const saveToLocalStorage = () => {
+        localStorage.setItem('formData', JSON.stringify(formData));
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        try {
+            saveToLocalStorage();
+        } catch (error) {
+
+        }
         console.log("form submitted");
         // if (step >= 1 && step <= 4) {
         setStep((prev) => prev + 1);
@@ -73,13 +103,17 @@ const PersonalLoanForm = () => {
 
     }
     const prevButton = () => setStep((prev) => prev - 1)
+    const nextButton = () => setStep((prev) => prev + 1)
     return (
         <div className="border p-6 m-4">
             {/* <Stepper /> */}
-            <h1 className="text-2xl font-semibold text-slate-800 my-4">Persoanl Loan Form</h1>
+            <div className="mb-8">
+                <Formstepper step={step} />
+            </div>
+
 
             <div>
-                {stepper(step, formData, setFormData, handleSubmit, prevButton)}
+                {stepper(step, formData, setFormData, handleSubmit, prevButton, nextButton, showPreview, setShowPreview)}
             </div>
             {/* <CommonForm
                 formControls={PersonalLoanFormDetails}
--- components/common/Form.jsx
+++ components/common/Form.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import {
@@ -9,6 +10,8 @@ import {
 } from "../ui/select";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import { Sparkle } from "lucide-react";
 
 function CommonForm({
     formControls,
@@ -19,89 +22,122 @@ function CommonForm({
     isBtnDisabled,
     step,
     prevButton,
+    fullwidth,
 }) {
+
     function renderInputsByComponentType(getControlItem) {
         let element = null;
         const value = formData[getControlItem?.name] || "";
+
         switch (getControlItem.componentType) {
             case "input":
                 element = (
-                    <Input
-                        name={getControlItem.name}
-                        placeholder={getControlItem.placeholder}
-                        id={getControlItem.name}
-                        type={getControlItem.type}
-                        value={value}
-                        onChange={(event) =>
-                            setFormData({
-                                ...formData,
-                                [getControlItem.name]: event.target.value,
-                            })
-                        }
-                    />
+                    <div>
+                        <Label className="mb-1">{getControlItem.label}</Label>
+                        <Input
+                            name={getControlItem.name}
+                            placeholder={getControlItem.placeholder}
+                            id={getControlItem.name}
+                            type={getControlItem.type}
+                            value={value}
+                            onChange={(event) =>
+                                setFormData({
+                                    ...formData,
+                                    [getControlItem.name]: event.target.value,
+                                })
+                            }
+                        />
+                    </div>
+
                 );
 
                 break;
             case "select":
                 element = (
-                    <Select
-                        onValueChange={(value) =>
-                            setFormData({
-                                ...formData,
-                                [getControlItem.name]: value,
-                            })
-                        }
-                        value={value}
-                    >
-                        <SelectTrigger className="w-full">
-                            <SelectValue placeholder={getControlItem.label} />
-                        </SelectTrigger>
-                        {/* <SelectContent>
-                            {getControlItem.options && getControlItem.options.length > 0
-                                ? getControlItem.options.map((optionItem, index) => (
-                                    <SelectItem key={index} value={optionItem.id}>
-                                        {optionItem.label}
-                                    </SelectItem>
-                                ))
-                                : null}
-                        </SelectContent> */}
-                    </Select>
+                    <div>
+                        <Label className="mb-1">{getControlItem.label}</Label>
+                        <Select
+                            onValueChange={(value) =>
+                                setFormData({
+                                    ...formData,
+                                    [getControlItem.name]: value,
+                                })
+                            }
+                            value={value}
+                        >
+                            <SelectTrigger className="w-full">
+                                <SelectValue placeholder={getControlItem.label} />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {getControlItem.options && getControlItem.options.length > 0
+                                    ? getControlItem.options.map((optionItem, index) => (
+                                        <SelectItem key={index} value={optionItem.id}>
+                                            {optionItem.label}
+                                        </SelectItem>
+                                    ))
+                                    : null}
+                            </SelectContent>
+                        </Select>
+                    </div>
+
                 );
 
                 break;
             case "textarea":
                 element = (
-                    <Textarea
-                        name={getControlItem.name}
-                        placeholder={getControlItem.placeholder}
-                        id={getControlItem.id}
-                        value={value}
-                        onChange={(event) =>
-                            setFormData({
-                                ...formData,
-                                [getControlItem.name]: event.target.value,
-                            })
-                        }
-                    />
+                    <div>
+                        <Label className="mb-1">{getControlItem.label}</Label>
+                        <Textarea
+                            name={getControlItem.name}
+                            placeholder={getControlItem.placeholder}
+                            id={getControlItem.id}
+                            value={value}
+                            onChange={(event) =>
+                                setFormData({
+                                    ...formData,
+                                    [getControlItem.name]: event.target.value,
+                                })
+                            }
+                        />
+                    </div>
+
                 );
 
+                break;
+            case "radio":
+                element = (
+                    <div className="flex items-center gap-2">
+                        <Label className="text-sm ml-3 font-medium flex items-center gap-4 "><Sparkle size={16} /> {getControlItem?.label}</Label>
+                        {getControlItem.options?.map((option, index) => (
+                            <div key={index} className="flex items-center cursor-pointer">
+                                <input
+                                    type="radio"
+                                    name={getControlItem.name}
+                                    id={option.value}
+                                    value={option.value}
+                                    checked={value === option.value}
+                                    className="cursor-pointer text-xs"
+                                    onChange={(event) =>
+                                        setFormData({
+                                            ...formData,
+                                            [getControlItem.name]: event.target.value,
+                                        })
+                                    }
+                                />
+                                <Label htmlFor={option.value} className="ml-1 cursor-pointer text-xs">
+                                    {option.label}
+                                </Label>
+                            </div>
+                        ))}
+                    </div>
+                );
                 break;
 
             default:
                 element = (
-                    <Input
-                        name={getControlItem.name}
-                        placeholder={getControlItem.placeholder}
-                        id={getControlItem.name}
-                        type={getControlItem.type}
-                        // value={value}
-                        onChange={(event) =>
-                            setFormData({
-                                ...formData,
-                                [getControlItem.name]: event.target.value,
-                            })
-                        }
-                    />
+                    <div className=" ">
+                        <p className="text-sm font-semibold mt-1 underline leading-7">{getControlItem?.label} </p>
+                    </div>
                 );
                 break;
         }
@@ -112,26 +148,31 @@ function CommonForm({
     return (
         <>
             <form onSubmit={onSubmit}>
-                <div className={`${formControls?.length <= 5 ? "grid-cols-2 " : "grid-cols-3 "} grid gap-3`}>
+                <div className={`grid gap-4 ${fullwidth ? "grid-cols-1" : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"} `}>
                     {formControls?.map((controlItem, index) => (
                         <div className="grid w-full gap-2" key={index}>
-                            <Label className="mb-1">{controlItem.label}</Label>
                             {renderInputsByComponentType(controlItem)}
                         </div>
                     ))}
                 </div>
                 <div className="flex justify-end w-full gap-4 mt-5">
                     {
-                        step > 1 ?
-                            <Button disabled={isBtnDisabled} variant="outline" type="button" onClick={prevButton} className="mt-5 ">
-                                Back
-                            </Button>
+                        buttonText ?
+                            <>
+                                {
+                                    step > 1 ?
+                                        <Button disabled={isBtnDisabled} variant="outline" type="button" onClick={prevButton} className="mt-5 ">
+                                            Back
+                                        </Button>
+                                        : null
+                                }
+                                <Button disabled={isBtnDisabled} type="submit" className="mt-5 ">
+                                    {buttonText ? buttonText : null}
+                                </Button>
+                            </>
                             : null
                     }
 
-                    <Button disabled={isBtnDisabled} type="submit" className="mt-5 ">
-                        {buttonText || "Submit"}
-                    </Button>
                 </div>
             </form>
         </>
