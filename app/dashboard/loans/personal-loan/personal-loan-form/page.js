041408967aa1f16df51d8c399f27eff5a7bf3379
Rishab49 <rishab.raj49@gmail.com>
2025-01-03 22:27:59 +0530

    refactored UI and client side logic

--- app/dashboard/loans/personal-loan/personal-loan-form/page.js
+++ app/dashboard/loans/personal-loan/personal-loan-form/page.js
@@ -1,93 +1,124 @@
 'use client'
 import React, { useState } from "react";
-import { PersoanlInfo, PersonalLoanFormDetails } from '@/config/PersoanlLoan';
-import CommonForm from "@/components/common/Form";
-import Stepper from '@/components/ui/stepper';
-import Step2 from '@/components/persoanlLon/step2';
-import Step1 from '@/components/persoanlLon/step1';
-import Step3 from '@/components/persoanlLon/step3';
-import FormPreview from '@/components/common/FormPreview';
-import FormPreviewPage from '@/components/common/Previewpage';
+import { PersonalLoan } from '@/config/PersoanlLoan';
 import Formstepper from '@/components/common/Formstepper';
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator"
+import { Label } from "@/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Button } from "@/components/ui/button"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
-const stepper = (step, formData, setFormData, handleSubmit, prevButton, nextButton, showPreview, setShowPreview) => {
-    switch (step) {
-        case 1: return (
-            <Step1
-                formControls={PersoanlInfo}
-                formData={formData}
-                setFormData={setFormData}
-                buttonText={"Next"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-                step={step}
-                nextButton={nextButton}
-                prevButton={prevButton}
-            />
-        )
-        case 2: return (
-            <Step2
-                formData={formData}
-                setFormData={setFormData}
-                buttonText={"Next"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-                step={step}
-                prevButton={prevButton}
 
-            />
-        )
-        case 3: return (
-            <Step3
-                formData={formData}
-                setFormData={setFormData}
-                buttonText={"Next"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-                step={step}
-                prevButton={prevButton}
 
-            />
-        )
-        case 4: return (
-            <FormPreviewPage
-                showPreview={showPreview}
-                setShowPreview={setShowPreview}
-                // formControls={PersonalLoanFormDetails}
-                // formData={formData}
-                setFormData={setFormData}
-                buttonText={"Next"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-                step={step}
-            />
-        )
-        // case 4: return (
-        //     <CommonForm
-        //         formControls={PersonalLoanFormDetails}
-        //         formData={formData}
-        //         setFormData={setFormData}
-        //         buttonText={"Submit"}
-        //         onSubmit={handleSubmit}
-        //         isBtnDisabled={false}
-        //         step={step}
-        //     />
-        // )
+const Step = ({ state, setState, step }) => {
+    return <div className="flex flex-col gap-8">
+        {
+            state[Object.keys(state)[step]].sections.map((section, i) => {
+                return <section className="flex flex-col gap-4" key={"section-" + i}>
+                    <div className="flex flex-col gap-2">
+                        <h2>{section.title}</h2>
+                        <Separator className="w-full" />
+                    </div>
+                    <div className="grid grid-cols-3 gap-6">
+                        {section.fields.map((field, ind) => {
+                            switch (field.type) {
+                                case "String":
+                                    return <div className="flex flex-col gap-2" key={"field-" + section + "" + ind}>
+                                        <Label htmlFor={field.name}>{field.label}</Label>
+                                        <Input id={field.name} value={field.value} onChange={(e) => {
+                                            setState((state) => {
+                                                state[step].sections[i].fields[ind].value = e.target.value;
+                                                return { ...state };
+                                            });
+                                        }} />
+                                    </div>;
+                                case "Date":
+                                    return <div className="flex flex-col gap-2">
+                                        <Label htmlFor={field.name}>{field.label}</Label>
+                                        <Input type="date" />
+                                    </div>;
+                                case "Option":
+                                    return <div className="flex flex-col gap-2 w-full">
+                                        <Label htmlFor={field.name}>{field.label}</Label>
+                                        <Select id={field.name} >
+                                            <SelectTrigger className="w-full">
+                                                <SelectValue placeholder={field.options[0].label} />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                    {
+                                                        field.options.map(option => <SelectItem value={option.label} key={option.label}>{option.label}</SelectItem>)
+                                                    }
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>;
+                                case "Binary":
+                                    return <div className="flex flex-col gap-2">
+                                        <Label htmlFor={field.name}>{field.label}</Label>
+                                        <RadioGroup defaultValue="comfortable" className="flex flex-row gap-4 items-center">
+                                            <div className="flex items-center space-x-2">
+                                                <Label><RadioGroupItem value="default" /> Yes</Label>
+                                            </div>
+                                            <div className="flex items-center space-x-2">
+                                                <Label><RadioGroupItem value="comfortable" id="r2" /> No</Label>
+                                            </div>
+                                        </RadioGroup>
+                                    </div>
+                                default:
+                                    return <div className="flex flex-col gap-2">
+                                        <Label htmlFor={field.name}>{field.label}</Label>
+                                        <Input id={field.name} value={field.value} onChange={(e) => {
+                                            setState((state) => {
+                                                state[step].sections[i].fields[ind].value = e.target.value;
+                                                return { ...state };
+                                            });
+                                        }} />
+                                    </div>;
+                            }
+                        })}
+                    </div>
 
-        default:
-            break;
-    }
+                </section>
+            })
+        }
+
+    </div>
 }
 
+const StepForm = ({
+    state, setState, step
+}) => {
+    return <Step state={state} setState={setState} step={step}></Step>
+
+}
+
+
 const PersonalLoanForm = () => {
-    const [step, setStep] = useState(1);
 
-    const [formData, setFormData] = useState([]);
-    const [showPreview, setShowPreview] = useState(true);
 
-    const saveToLocalStorage = () => {
-        localStorage.setItem('formData', JSON.stringify(formData));
-    };
+    // step denoting the current step of the form
+    const [step, setStep] = useState(0);
+
+    // state representing the formData
+    const [state, setState] = useState(PersonalLoan);
+
+    // number of steps in form
+    const stepLength = Object.keys(state).length;
+
+
+
+    // const saveToLocalStorage = () => {
+    //     localStorage.setItem('formData', JSON.stringify(formData));
+    // };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -102,28 +133,24 @@ const PersonalLoanForm = () => {
         // }
 
     }
-    const prevButton = () => setStep((prev) => prev - 1)
-    const nextButton = () => setStep((prev) => prev + 1)
+
     return (
         <div className="border p-6 m-4">
-            {/* <Stepper /> */}
             <div className="mb-8">
-                <Formstepper step={step} />
+                <Formstepper step={step} keys={Object.keys(state).map(key => state[key].title)} />
             </div>
+            <StepForm state={state} setState={setState} step={step} />
+
+            <div className="py-8 flex flex-row items-center justify-end gap-4">
+                {
+                    step == 0 ? null : <Button type="button" variant="outline" onClick={() => setStep(state => state - 1)}>previous</Button>
+                }
+                {
+                    step >= stepLength - 1 ? null : <Button type="button" onClick={() => setStep(state => state + 1)}>next</Button>
+                }
 
 
-            <div>
-                {stepper(step, formData, setFormData, handleSubmit, prevButton, nextButton, showPreview, setShowPreview)}
             </div>
-            {/* <CommonForm
-                formControls={PersonalLoanFormDetails}
-                formData={formData}
-                setFormData={setFormData}
-                buttonText={"Submit"}
-                onSubmit={handleSubmit}
-                isBtnDisabled={false}
-
-            /> */}
         </div>
     )
 }
